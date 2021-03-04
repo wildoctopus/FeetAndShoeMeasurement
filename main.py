e970205625b4692813ba2576ecca28fc6770bab9
Alok Pandey <akphacker@live.com>
2021-03-04 14:29:22 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,59 @@
+from sklearn.cluster import KMeans
+import random as rng
+import cv2
+import imutils
+import argparse
+from imutils import contours
+from skimage.io import imread
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+from utils import *
+
+
+ImgPath = 'data/barefeet1.jpeg'
+
+
+
+
+def main():
+
+    oimg = imread(ImgPath)
+
+    if not os.path.exists('output'):
+        os.makedirs('output')
+
+
+
+    preprocessedOimg = preprocess(oimg)
+    cv2.imwrite('output/preprocessedOimg.jpg', preprocessedOimg)
+
+    clusteredImg = kMeans_cluster(preprocessedOimg)
+    cv2.imwrite('output/clusteredImg.jpg', clusteredImg)
+
+    edgedImg = edgeDetection(clusteredImg)
+    cv2.imwrite('output/edgedImg.jpg', edgedImg)
+
+    boundRect, contours, contours_poly, img = getBoundingBox(edgedImg)
+    pdraw = drawCnt(boundRect[1], contours, contours_poly, img)
+    cv2.imwrite('output/pdraw.jpg', pdraw)
+
+
+    croppedImg, pcropedImg = cropOrig(boundRect[1], clusteredImg)
+    cv2.imwrite('output/croppedImg.jpg', croppedImg)
+    
+
+    newImg = overlayImage(croppedImg, pcropedImg)
+    cv2.imwrite('output/newImg.jpg', newImg)
+
+    fedged = edgeDetection(newImg)
+    fboundRect, fcnt, fcntpoly, fimg = getBoundingBox(fedged)
+    fdraw = drawCnt(fboundRect[2], fcnt, fcntpoly, fimg)
+    cv2.imwrite('output/fdraw.jpg', fdraw)
+
+    print("feet size: ", calcFeetSize(pcropedImg, fboundRect))
+
+
+if __name__ == '__main__':
+    main()
